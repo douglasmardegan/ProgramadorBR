e2d44f748f44eafdcc5f6982afd86cc0967bc16c
Douglas Mardegan Colodete <douglasmardegan@yahoo.com.br>
2022-01-26 17:34:03 -0300

    3rd improvement

--- Conversor de moedas/currencyConverter.js
+++ Conversor de moedas/currencyConverter.js
@@ -26,6 +26,26 @@ function updateData() {
     initialSelectedCurrency_stringSliced;
   document.getElementById("finalCurrency").innerHTML =
     finalSelectedCurrency_stringSliced;
+}
+
+function convertCurrency() {
+  let initialSelectedCurrency = document.getElementById(
+    "initialSelectedCurrency"
+  );
+  let initialSelectedCurrency_string =
+    initialSelectedCurrency.options[initialSelectedCurrency.selectedIndex].text;
+
+  let initialSelectedCurrency_stringSliced =
+    initialSelectedCurrency_string.slice(0, 3);
+
+  let finalSelectedCurrency = document.getElementById("finalSelectedCurrency");
+  let finalSelectedCurrency_string =
+    finalSelectedCurrency.options[finalSelectedCurrency.selectedIndex].text;
+
+  let finalSelectedCurrency_stringSliced = finalSelectedCurrency_string.slice(
+    0,
+    3
+  );
 
   let initialUrl = "https://economia.awesomeapi.com.br/last/";
 
@@ -34,27 +54,30 @@ function updateData() {
     initialSelectedCurrency_stringSliced +
     "-" +
     finalSelectedCurrency_stringSliced;
-}
 
-function convertCurrency() {
-  let inputtedValueToConvert = document.getElementById("inputValue");
-  let inputtedValueToConvert_amended = parseFloat(
+  let inputtedValueToConvert = document.querySelector(".inputValue").value;
+  let inputtedValueToConvert_amended = `${parseFloat(
     inputtedValueToConvert
-  ).toFixed(2);
+  ).toFixed(2)}`;
 
   fetch(finalUrl)
     .then((res) => {
       return res.json();
     })
     .then((data) => {
-      let currencyQuote = parseFloat(data.currencyTypes.high).toFixed(2);
-      console.log(currencyQuote);
-
-      // Para acessar a maior cotação fazer: console.log(data["USDBRL"]["high"]);
-      // Ou: console.log(data.USDBRL.high);
-      let currencyConversionResult = `${parseFloat(
-        inputtedValueToConvert_amended * currencyQuote
+      let currencyTypes =
+        initialSelectedCurrency_stringSliced +
+        finalSelectedCurrency_stringSliced;
+      let currencyHighQuote = parseFloat(data[currencyTypes].bid);
+      let currencyLowQuote = parseFloat(data[currencyTypes].ask);
+      let currencyMeanQuote = `${(
+        (currencyHighQuote + currencyLowQuote) /
+        2
       ).toFixed(2)}`;
+
+      let currencyConversionResult = (
+        inputtedValueToConvert_amended * currencyMeanQuote
+      ).toFixed(2);
 
       document.getElementById("currencyConversionResult").innerHTML =
         currencyConversionResult;
